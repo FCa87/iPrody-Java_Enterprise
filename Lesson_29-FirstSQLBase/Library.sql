04890a17fb15f711eeaaf7bb542d14f62e268844
Андрей.Казаков <fc-study@yandex.ru>
2026-02-05 00:36:57 +0300

    My_first_SQL_DB

---
+++ Lesson_29-FirstSQLBase/Library.sql
@@ -0,0 +1,34 @@
+CREATE SCHEMA Library;
+
+CREATE TABLE IF NOT EXISTS Library.books
+(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    published_year INT CHECK (published_year > 0),
+    genre VARCHAR(100)
+)
+
+CREATE TABLE IF NOT EXISTS Library.readers
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE,
+    phone VARCHAR(15) UNIQUE
+)
+
+CREATE TABLE IF NOT EXISTS Library.borrowed_books
+(
+    id SERIAL PRIMARY key,
+    book_id INT NOT null,
+    reader_id INT NOT null,
+    borrow_date DATE NOT NULL,
+    return_date DATE,
+    status VARCHAR(20) CHECK (status IN ('borrowed', 'returned')),
+    FOREIGN KEY (book_id)  REFERENCES Library.books (id) ON DELETE SET NULL,
+    FOREIGN KEY (reader_id)  REFERENCES Library.readers (id) ON DELETE SET null,
+)
+
+CREATE INDEX title_hash_idx ON Library.books using hash (title);
+
+CREATE INDEX reader_id_idx ON Library.borrowed_books (reader_id) WHERE (status = 'borrowed');
